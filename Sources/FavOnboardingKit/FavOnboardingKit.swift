b7679cd205d46f06a8503336138d56782d8d1459
kelvinfok <punkd1@gmail.com>
2022-03-20 00:39:00 +0800

    add onboarding view controller

--- Sources/FavOnboardingKit/FavOnboardingKit.swift
+++ Sources/FavOnboardingKit/FavOnboardingKit.swift
@@ -3,13 +3,19 @@ import UIKit
 
 public class FavOnboardingKit {
   
+  private var onboardingViewController: OnboardingViewController = {
+    let controller = OnboardingViewController()
+    controller.modalTransitionStyle = .crossDissolve
+    controller.modalPresentationStyle = .fullScreen
+    return controller
+  }()
   
   public init() {
     
   }
   
   public func launchOnboarding(rootVC: UIViewController) {
-    
+    rootVC.present(onboardingViewController, animated: true, completion: nil)
   }
   
   public func dismissOnboarding() {
---
+++ Sources/FavOnboardingKit/controller/OnboardingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kelvin Fok on 20/3/22.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
