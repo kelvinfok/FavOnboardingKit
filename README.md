ef464ecca5eba55a08736e328b18be286b377aa6
kelvinfok <punkd1@gmail.com>
2022-03-19 23:40:42 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FavOnboardingKit
+
+A description of this package.
---
+++ Tests/FavOnboardingKitTests/FavOnboardingKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FavOnboardingKit
+
+final class FavOnboardingKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FavOnboardingKit().text, "Hello, World!")
+    }
+}
